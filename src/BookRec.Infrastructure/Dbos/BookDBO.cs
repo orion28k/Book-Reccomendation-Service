540cc00bb0762863eef1945fbcb6a32ae149bb6d
Briana Davis <brianad570@gmail.com>
2025-12-01 21:00:49 -0500

    Created Book and User DBOs

---
+++ src/BookRec.Infrastructure/Dbos/BookDBO.cs
@@ -0,0 +1,12 @@
+namespace BookRec.Infrastructure.Dbos;
+
+public class BookDBO
+{
+     public string Title { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Genre { get; set; } = string.Empty;
+    public DateTime PublishDate { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
